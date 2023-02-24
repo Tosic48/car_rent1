bec9c48f535f96e38dea5ef9e4b11f8501a6fe56
WorkPC <nikmarooo@mail.ru>
2023-02-24 17:04:01 +0300

    1

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from webapp.views import index, about
+from webapp.views import index, about, services,cars
 
 app_name = 'webapp'
 
 urlpatterns = [
     path('', index, name='home'),
-    path('about/', about, name= 'about'),
+    path('about/', about, name='about'),
+    path('services/', services, name='services'),
+    path('cars/', cars, name='cars'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -3,5 +3,18 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'webapp/index.html')
 
+
 def about(request):
     return render(request, 'webapp/about.html')
+
+
+def services(request):
+    return render(request, 'webapp/services.html')
+
+
+def contact(request):
+    return render(request, 'webapp/contact.html')
+
+
+def cars(request):
+    return render(request, 'webapp/cars.html')
